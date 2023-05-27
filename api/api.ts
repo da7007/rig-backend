ac48cb2501e6c583a00076423768571951d8a870
Dylan Armstrong <dylan@Dylans-MBP.TUX>
2023-05-28 03:30:52 +1000

    Added DB connectivity for logins

--- api/api.ts
+++ api/api.ts
@@ -1,16 +1,39 @@
 import express from "express";
 import cors from "cors";
+import * as mongo from "mongodb";
+import dotenv from "dotenv";
 
 const app = express();
 const port: number = 3000;
+
+dotenv.config();
+
+async function connectToDatabase(collectionName: string, email?: string, password?: string): Promise<Boolean> {
+    const client: mongo.MongoClient = new mongo.MongoClient(process.env.DB_CONNECTION as string);
+    await client.connect();
+    const db: mongo.Db = client.db(process.env.DB_NAME)
+    const collection = db.collection(collectionName);
+    const item = await collection.findOne({ email_address: email });
+    if(password === item?.password) {
+        return true;
+    } else {
+        return false;
+    } 
+}
 
 app.use(cors({
     methods: ["GET", "POST"],
     origin: "*"
 }))
 
-app.get("/login", (req, res) => {
-    res.send(200);
+app.post("/login", async (req, res) => {
+    const reqEmailAddress: string = req.query.email_address as string;
+    const reqPassword: string = req.query.password as string;
+    if(await connectToDatabase("Users", reqEmailAddress, reqPassword)) {
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(403);
+    }  
 });
 
 app.listen(port, () => {
---
+++ api/types.ts
@@ -0,0 +1,10 @@
+import * as mongo from "mongodb";
+
+export interface User {
+    _id: mongo.ObjectId;
+    first_name: string;
+    last_name: string;
+    username: string;
+    email_address: string;
+    password: string;
+}
